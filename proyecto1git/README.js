4002a04f0390698d4e716d83c13b5b2f8c822799
ifherh <42616858+ifherh@users.noreply.github.com>
2018-09-27 20:45:47 -0500

    Update README.js

--- proyecto1git/README.js
+++ proyecto1git/README.js
@@ -1,3 +1,3 @@
 "proyecto "
 "tiene otr cosa que va a causar conflicto, antes ea un subtitulo para los dos"
-#esto es un comentario en los dos
+#esto es un comentario en los dos, pero se cambio en master desde github
